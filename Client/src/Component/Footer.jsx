08b253af871de5d4809d7ea05114c0aef1b97488
praveen choudhary <pc852180@gmail.com>
2025-07-02 13:13:57 +0530

    Airbirds

--- Client/src/Component/Footer.jsx
+++ Client/src/Component/Footer.jsx
@@ -2,7 +2,7 @@
 const Footer = ()=>{
     return(
         <>
-        <h1 style={{padding:"90px", textAlign:"center",fontWeight:"bold",backgroundColor:"lightpink" }} >www.airbirds.com</h1>
+        <h3 style={{padding:"90px", textAlign:"center",fontWeight:"bold",backgroundColor:"lightgrey", textTransform:"uppercase"}} >www.airbirds.com</h3>
         </>
     )
 }
