cd609e9e2fad70f6d3f755512838048477e2e7a8
praveen choudhary <pc852180@gmail.com>
2025-07-02 13:09:49 +0530

    Airbirds

---
+++ Client/src/Component/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = ()=>{
+    return(
+        <>
+        <h1 style={{padding:"90px", textAlign:"center",fontWeight:"bold",backgroundColor:"lightpink" }} >www.airbirds.com</h1>
+        </>
+    )
+}
+
+export default Footer;
---
+++ Client/src/Pages/Registration.jsx
@@ -0,0 +1,11 @@
+
+
+const Registration = ()=>{
+    return(
+        <>
+        <h1>Regsitration User Page</h1>
+        </>
+    )
+}
+
+export default Registration;
