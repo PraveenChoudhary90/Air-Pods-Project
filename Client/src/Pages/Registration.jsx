f2bf30f18b636ed4fdef696d165f2f16ddaeb2c5
praveen choudhary <pc852180@gmail.com>
2025-07-02 16:04:08 +0530

    Airbirds

--- Client/src/Pages/Registration.jsx
+++ Client/src/Pages/Registration.jsx
@@ -1,4 +1,5 @@
-
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Registration = ()=>{
     return(
@@ -8,7 +9,24 @@ const Registration = ()=>{
          Offer:All Products have 15% Discount in Dewali Festival
         </marquee>
         </h1>
-        <h1>Regsitration User Page</h1>
+
+        <div id="from">
+        <h3>Regsitration User Page</h3>
+        <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+    </div>
         </>
     )
 }
