a584b958cdc2006216b01a610efb3a554e31c0e6
praveen choudhary <pc852180@gmail.com>
2025-07-03 16:52:01 +0530

    Air-pods

--- Client/src/Component/TopNav.jsx
+++ Client/src/Component/TopNav.jsx
@@ -5,8 +5,21 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import { FaRegUser } from "react-icons/fa";
+import { FaUserCircle } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
 
 const TopNav = ()=>{
+  const [input, setInput] = useState("");
+   
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+
     return(
         <>
         <div id="nav">
@@ -24,6 +37,12 @@ const TopNav = ()=>{
             <Nav.Link  as={Link}  to="about">About</Nav.Link>
             <Nav.Link  as={Link}  to="registration">Registration</Nav.Link>
           </Nav>
+          <div id="icon">
+          <FaRegUser />
+          <FaHeart />
+          <FaUserCircle onClick={handleShow} />
+          </div>
+
           <Form className="d-flex">
             <Form.Control
               type="search"
@@ -37,6 +56,44 @@ const TopNav = ()=>{
       </Container>
     </Navbar>
     </div>
+
+
+
+   {/* Admin model */}
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Admin Login</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email"/>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit" >
+        Submit
+      </Button>
+    </Form>
+          </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
 
         </>
     )
