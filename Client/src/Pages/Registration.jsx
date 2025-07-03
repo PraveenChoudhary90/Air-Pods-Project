08cc21ab32196553ddca37fb054fe0b73f25cdb7
praveen choudhary <pc852180@gmail.com>
2025-07-03 14:48:38 +0530

    Air-pods

--- Client/src/Pages/Registration.jsx
+++ Client/src/Pages/Registration.jsx
@@ -1,7 +1,23 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 const Registration = ()=>{
+    
+  const [input, setInput]  =useState("");
+
+  const HandelInput = (e)=>{
+    const name = e.target.name;
+    const value = e.target.value;
+    setInput(values=>({...values, [name]:value}));
+    console.log(input);
+}
+
+const HandelSubmit = async(e)=>{
+  e.preventDefault();
+}
+
+
     return(
         <>
         <h1 style={{backgroundColor:"lightpink"}}>
@@ -15,14 +31,28 @@ const Registration = ()=>{
         <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Enter Name</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="text" name='name' value={input.name} onChange={HandelInput} />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Label>Enter Email</Form.Label>
+        <Form.Control type="text" name='name' value={input.name} onChange={HandelInput}/>
       </Form.Group>
-      <Button variant="primary" type="submit">
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Enter City</Form.Label>
+        <Form.Control type="text" name='name' value={input.name} onChange={HandelInput} />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Enter Number</Form.Label>
+        <Form.Control type="text"name='name' value={input.name} onChange={HandelInput} />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Enter Password</Form.Label>
+        <Form.Control type="password" name='name' value={input.name} onChange={HandelInput}/>
+      </Form.Group>
+      <Button variant="primary" type="submit" onClick={HandelSubmit}>
         Submit
       </Button>
     </Form>
