908a88a04a9b068bbd02ccf5057ccdabc972637c
praveen choudhary <pc852180@gmail.com>
2025-07-03 13:28:49 +0530

    Air-pods

--- Client/src/Pages/About.jsx
+++ Client/src/Pages/About.jsx
@@ -1,16 +1,134 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
     <>
-       <h1 style={{backgroundColor:"lightpink"}}>
+      <h1 style={{ backgroundColor: "lightpink" }}>
         <marquee behavior="alternate" direction="left">
-         Offer:All Products have 15% Discount in Dewali Festival
+          Offer:All Products have 15% Discount in Dewali Festival
         </marquee>
-        </h1>
-    <h1>About Page</h1>
+      </h1>
+      <div id="ab1">
+        <img src="ab1.jpg" alt="" width="600px" height="500px" />
+        <h4 style={{ width: "500px" }}>
+          AirPods are Apple's line of wireless Bluetooth earbuds, known for
+          their seamless integration with Apple devices and convenient features
+          like automatic pairing, switching between devices, and hands-free Siri
+          access. They offer various features like Active Noise Cancellation,
+          Personalized Spatial Audio, and Hearing Health features, depending on
+          the specific model. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Reprehenderit optio soluta deserunt suscipit odio assumenda
+          sapiente modi corporis laboriosam pariatur temporibus dolores dolor
+          ipsum, accusantium numquam, maxime natus, blanditiis nostrum!
+        </h4>
+      </div>
+
+      <div id="ab2">
+        <h4 style={{ width: "500px" }}>
+          AirPods are Apple's line of wireless Bluetooth earbuds, known for
+          their seamless integration with Apple devices and convenient features
+          like automatic pairing, switching between devices, and hands-free Siri
+          access. They offer various features like Active Noise Cancellation,
+          Personalized Spatial Audio, and Hearing Health features, depending on
+          the specific model. Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Maiores nesciunt facere qui porro explicabo dolores,
+          blanditiis placeat repellendus illo nihil soluta distinctio ratione
+          minus, cupiditate ipsum rerum praesentium. Doloremque, repellendus.
+        </h4>
+        <img src="ab2.jpg" alt="" width="600px" height="500px" />
+      </div>
+
+      <div id="ab3">
+        <img src="ab3.jpg" alt="" width="600px" height="500px" />
+        <h4 style={{ width: "500px" }}>
+          AirPods are Apple's line of wireless Bluetooth earbuds, known for
+          their seamless integration with Apple devices and convenient features
+          like automatic pairing, switching between devices, and hands-free Siri
+          access. They offer various features like Active Noise Cancellation,
+          Personalized Spatial Audio, and Hearing Health features, depending on
+          the specific model. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Reprehenderit optio soluta deserunt suscipit odio assumenda
+          sapiente modi corporis laboriosam pariatur temporibus dolores dolor
+          ipsum, accusantium numquam, maxime natus, blanditiis nostrum!
+        </h4>
+      </div>
+
+       <div id="ab4">
+        <h4 style={{ width: "500px" }}>
+          AirPods are Apple's line of wireless Bluetooth earbuds, known for
+          their seamless integration with Apple devices and convenient features
+          like automatic pairing, switching between devices, and hands-free Siri
+          access. They offer various features like Active Noise Cancellation,
+          Personalized Spatial Audio, and Hearing Health features, depending on
+          the specific model. Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Maiores nesciunt facere qui porro explicabo dolores,
+          blanditiis placeat repellendus illo nihil soluta distinctio ratione
+          minus, cupiditate ipsum rerum praesentium. Doloremque, repellendus.
+        </h4>
+        <img src="ab4.jpg" alt="" width="600px" height="500px" />
+      </div>
+
+      <div id="ab3">
+        <img src="ab5.jpg" alt="" width="600px" height="500px" />
+        <h4 style={{ width: "500px" }}>
+          AirPods are Apple's line of wireless Bluetooth earbuds, known for
+          their seamless integration with Apple devices and convenient features
+          like automatic pairing, switching between devices, and hands-free Siri
+          access. They offer various features like Active Noise Cancellation,
+          Personalized Spatial Audio, and Hearing Health features, depending on
+          the specific model. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Reprehenderit optio soluta deserunt suscipit odio assumenda
+          sapiente modi corporis laboriosam pariatur temporibus dolores dolor
+          ipsum, accusantium numquam, maxime natus, blanditiis nostrum!
+        </h4>
+      </div>
+
+      <div id="ab4">
+        <h4 style={{ width: "500px" }}>
+          AirPods are Apple's line of wireless Bluetooth earbuds, known for
+          their seamless integration with Apple devices and convenient features
+          like automatic pairing, switching between devices, and hands-free Siri
+          access. They offer various features like Active Noise Cancellation,
+          Personalized Spatial Audio, and Hearing Health features, depending on
+          the specific model. Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Maiores nesciunt facere qui porro explicabo dolores,
+          blanditiis placeat repellendus illo nihil soluta distinctio ratione
+          minus, cupiditate ipsum rerum praesentium. Doloremque, repellendus.
+        </h4>
+        <img src="ab6.jpg" alt="" width="600px" height="500px" />
+      </div>
+
+      <div id="ab3">
+        <img src="ab7.jpg" alt="" width="600px" height="500px" />
+        <h4 style={{ width: "500px" }}>
+          AirPods are Apple's line of wireless Bluetooth earbuds, known for
+          their seamless integration with Apple devices and convenient features
+          like automatic pairing, switching between devices, and hands-free Siri
+          access. They offer various features like Active Noise Cancellation,
+          Personalized Spatial Audio, and Hearing Health features, depending on
+          the specific model. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Reprehenderit optio soluta deserunt suscipit odio assumenda
+          sapiente modi corporis laboriosam pariatur temporibus dolores dolor
+          ipsum, accusantium numquam, maxime natus, blanditiis nostrum!
+        </h4>
+      </div>
+
+      <div id="ab4">
+        <h4 style={{ width: "500px" }}>
+          AirPods are Apple's line of wireless Bluetooth earbuds, known for
+          their seamless integration with Apple devices and convenient features
+          like automatic pairing, switching between devices, and hands-free Siri
+          access. They offer various features like Active Noise Cancellation,
+          Personalized Spatial Audio, and Hearing Health features, depending on
+          the specific model. Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Maiores nesciunt facere qui porro explicabo dolores,
+          blanditiis placeat repellendus illo nihil soluta distinctio ratione
+          minus, cupiditate ipsum rerum praesentium. Doloremque, repellendus.
+        </h4>
+        <img src="bl6.jpg" alt="" width="600px" height="500px" />
+      </div>
     </>
-  )
+  );
 }
 
-export default About
+export default About;
