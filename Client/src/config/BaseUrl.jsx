d2ee43bcf3bf44b2bbed5d63652d073afc16eb7e
praveen choudhary <pc852180@gmail.com>
2025-07-10 21:32:18 +0530

    Air-Bluetooth

--- Client/src/config/BaseUrl.jsx
+++ Client/src/config/BaseUrl.jsx
@@ -1,6 +1,6 @@
 
 
-const BASE_URL = "http://localhost:8000";
-// const BASE_URL = "https://air-pods-project.onrender.com";
+// const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://air-pods-project.onrender.com";
 
 export default BASE_URL;
