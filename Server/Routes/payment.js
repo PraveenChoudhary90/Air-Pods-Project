78fda6ed12c44b9c076d82c633b1dd807b56a92e
praveen choudhary <pc852180@gmail.com>
2025-07-10 16:06:59 +0530

    Air-pods

--- Server/Routes/payment.js
+++ Server/Routes/payment.js
@@ -142,7 +142,7 @@
 const express = require("express");
 const Razorpay = require("razorpay");
 const crypto = require("crypto");
-const OrderModel = require("../model/OrderModel");
+const OrderModel = require("../Model/OrderModel");
 
 const router = express.Router();
 
